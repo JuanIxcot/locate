7f0d9cb9e9dabda0957cef795e547e1c531bc97a
JuanIxcot <160376397+JuanIxcot@users.noreply.github.com>
2024-03-03 12:40:49 -0600

    Add files via upload

---
+++ contribucion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main()
+class contribucion {
+	protected : string nombres,apellidos,direccion,fecha_nacimiento;
+				int telefono;
+	contribucion(){
+	}
+	contribucion(string nom, string ape, string dic, int tel, string fn){
+		nombres = nom;
+		apellidos = ape;
+		direccion = dic;
+		telefono = tel;
+		fecha_nacimiento = fn;
+	}
+	void crear();
+	void leer();
+	void actualiza();
+	void borrar();
+
+};
